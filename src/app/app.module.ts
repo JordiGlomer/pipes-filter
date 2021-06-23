4447bcf1fe21e6e946e8b5f75d08facb29463bba
Your Name <you@example.com>
2021-06-23 14:26:25 +0200

    a ver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FilterPipe } from './shared/pipesFilter/filter.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FilterPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'custom-pipes',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss']
+})
+export class CustomPipesComponent implements OnInit {
+  stuff:Array<Object>=[];
+  pattern = string;
+
+  constructor() {}
+
+  ngOnInit(): void{}
+
+  addItem(item) {
+    this.stuff.push({name: item});
+  }
+ // styles: []
+}
